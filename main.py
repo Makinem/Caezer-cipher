6428f9558f82987c24fd85fccf702c3bf4f087a8
Makinem <omoshovabubai@gmail.com>
2026-04-12 02:25:20 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def caesar_cipher(text, shift, mode="encrypt"):
+    result = ""
+    if mode == "decrypt":
+        shift = -shift
+    for char in text:
+        if char.isalpha():
+            start = ord('A') if char.isupper() else ord('a')
+            new_char = chr((ord(char) - start + shift) % 26 + start)
+            result += new_char
+        else:
+            result += char
+    return result
+print("Шифр Цезаря")
+text = input("Введите текст: ")
+shift = int(input("Введите сдвиг: "))
+mode = input("Выберите режим (encrypt/decrypt): ").lower()
+result = caesar_cipher(text, shift, mode)
+print("Результат:", result)
